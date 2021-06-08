24a378c994350d10df8070cf6cd11d1de4a6f0d3
André Kunde <andre.kunde@zup.com.br>
2021-06-08 08:34:39 -0300

    removing eslint and prettier

--- src/components/Tooltip/styles.ts
+++ src/components/Tooltip/styles.ts
@@ -10,8 +10,9 @@ export const Container = styled.div`
     border-radius: 4px;
     font-size: 14px;
     font-weight: 500;
-    /* opacity: 0;
-    transition: opacity 0.4s; */
+    opacity: 0;
+    transition: opacity 0.4s;
+    visibility: hidden;
 
     position: absolute;
     bottom: calc(100% + 12px);
@@ -21,11 +22,19 @@ export const Container = styled.div`
     color: #312e38;
 
     &::before {
+      content: '';
       border-style: solid;
-      border-color: #ff9000;
+      border-color: #ff9000 transparent;
       border-width: 6px 6px 0 6px;
       top: 100%;
       position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
     }
+  }
+
+  &:hover span {
+    opacity: 1;
+    visibility: visible;
   }
 `;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -4,7 +4,7 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
-import getValidationErrors from '../../utils/getValidationErros';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 
